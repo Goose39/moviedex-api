836e228ea562b2abdfa9baae4131528c18e0ee28
Goose39 <Goose39.Dev@gmail.com>
2019-12-27 15:54:14 -0500

    api authorization implemented

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors')
@@ -10,8 +11,18 @@ app.use(morgan('dev'));
 app.use(helmet())
 app.use(cors())
 
-function handleGetMovies(req, res) {
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
 
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+function handleGetMovies(req, res) {
   let response = movies
 
   if (req.query.genre) {
