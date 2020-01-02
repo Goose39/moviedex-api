f253f3b7773d394a0e4762b19de5902319d0e306
Goose39 <Goose39.Dev@gmail.com>
2020-01-02 15:50:19 -0500

    production ready code

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const movies = require('./movies.json')
 
 app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -48,7 +49,18 @@ function handleGetMovies(req, res) {
 
 app.get('/movie', handleGetMovies)
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
